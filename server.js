2827c1f2c6244ff1230f2dd75ee0b4726748bfd5
Samuel <samuel310597@gmail.com>
2020-12-30 12:23:07 +0100

    game done ✨

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-const { disconnect } = require('process');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -14,4 +13,53 @@ app.get('/', function(req, res) {
 });
 http.listen(3000, function() {
 	console.log('listening on *:3000');
+});
+
+
+var users = [];
+io.on('connect', function(socket) {
+    console.log('nouvel utilisateur : ' + socket.id);
+    var newUser = {
+        pseudo: null,
+        id: socket.id,
+        x: 0,
+        y: 0,
+        speed: 500,
+    }
+
+	users.push(newUser);
+
+    io.emit('initUser', newUser)
+    
+    io.emit('sendAllUser', users);
+
+	socket.on('disconnect', function() {
+        const user = users.find((element) => element.id == socket.id)
+        users.splice(users.indexOf(user), 1);
+        io.emit('sendAllUser', users);
+        console.log('Deconnection de '+ user.pseudo + " = " + socket.id);
+    });
+    
+    socket.on('UpdatePositions', function(hero) {
+        var user;
+        for(var i=0; i<users.length; ++i){
+            if(users[i].id == hero.id){
+                user = users[i];
+                users[i] = hero
+            }
+        }
+        io.emit('sendAllUser', users);
+    })
+
+    socket.on('UpdatePseudo', function(hero){
+        var user;
+        for(var i=0; i<users.length; ++i){
+            if(users[i].id == hero.id){
+                user = users[i];
+                users[i] = hero
+            }
+        }
+        io.emit('sendAllUser', users);
+    })
+
 });
